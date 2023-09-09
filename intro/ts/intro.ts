bf343cfef718c7580e8f8a4e74150e547fe6f39b
Mario Alvarez <marioalvarez.informatico@gmail.com>
2023-09-09 18:08:32 -0300

    clases TS

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -1,1 +1,19 @@
-console.log('Hello World');
+console.log("TypeScript is working!");
+// npm install -g typescript ts-node
+
+// clase
+class Drink {
+  private name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  info(): string {
+    return this.name;
+  }
+}
+
+const drink = new Drink("Coffee");
+console.log(drink.info());
+
+// herencia
+
