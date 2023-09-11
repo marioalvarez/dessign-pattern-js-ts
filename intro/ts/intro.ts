cd8426bdb9026703fe520d3ccf18d73bf534cec5
Mario Alvarez <marioalvarez.informatico@gmail.com>
2023-09-11 09:06:52 -0300

    interface

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -15,19 +15,58 @@ class Drink {
 const drink = new Drink("Coffee");
 console.log(drink.info());
 
-// herencia
-class Beer extends Drink{
-    private alcohol: number;
-
-    constructor(name: string, alcohol: number){
-        super(name);
-        this.alcohol = alcohol;
-    }
-
-    info(): string {
-        return super.info() + " " + this.alcohol;
-      }
+//interface
+interface Product {
+  price: number;
+  getPrice(): string;
 }
 
-const beer = new Beer("Mahou", 5);
+// herencia
+class Beer extends Drink implements Product {
+  private alcohol: number;
+  price: number;
+
+  constructor(name: string, alcohol: number, price: number) {
+    super(name);
+    this.alcohol = alcohol;
+    this.price = price;
+  }
+
+  getPrice(): string {
+    return "$ " + this.price;
+  }
+
+  info(): string {
+    return super.info() + " " + this.alcohol;
+  }
+}
+// implementacion de interfaz
+class Snack implements Product {
+  private name: string;
+  price: number;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+
+  getPrice(): string {
+    return "El precio es: $ " + this.price;
+  }
+}
+
+const beer = new Beer("Mahou", 5, 10);
 console.log(beer.info());
+
+const products: Product[] = [
+  new Beer("Mahou", 5, 10),
+  new Snack("Papas", 5),
+  new Beer("Coron", 5, 10),
+];
+
+
+function getPrices(items :Product[]) {
+  return items.map((item) => item.getPrice());
+}
+
+console.log(getPrices(products));
