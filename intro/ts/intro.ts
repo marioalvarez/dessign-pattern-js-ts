9acced6904c245216fc18be472767cf67d93eb63
Mario Alvarez <marioalvarez.informatico@gmail.com>
2023-09-10 23:15:35 -0300

    herencia TS

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -16,4 +16,18 @@ const drink = new Drink("Coffee");
 console.log(drink.info());
 
 // herencia
+class Beer extends Drink{
+    private alcohol: number;
 
+    constructor(name: string, alcohol: number){
+        super(name);
+        this.alcohol = alcohol;
+    }
+
+    info(): string {
+        return super.info() + " " + this.alcohol;
+      }
+}
+
+const beer = new Beer("Mahou", 5);
+console.log(beer.info());
